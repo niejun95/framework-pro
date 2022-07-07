950adbfcca7d9814179cbc6c256b4446ec133ffa
niejun <1289680419@qq.com>
2022-07-07 09:03:11 +0800

    redis template 序列化配置

---
+++ src/main/java/org/example/config/RedisTemplateConfig.java
@@ -0,0 +1,54 @@
+package org.example.config;
+
+import com.alibaba.fastjson.support.spring.GenericFastJsonRedisSerializer;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.serializer.RedisSerializer;
+import org.springframework.data.redis.serializer.StringRedisSerializer;
+
+/**
+ * @ClassName RedisTemplateConfig
+ * @Author niejun
+ * @Date 2022/7/7
+ * @Description: RedisTemplate 序列化器配置
+ * @Version 1.0
+ **/
+@Configuration
+public class RedisTemplateConfig {
+    @Bean
+    public RedisSerializer<String> redisKeySerializer() {
+        return new StringRedisSerializer();
+    }
+
+    @Bean
+    public RedisSerializer<Object> redisValueSerializer() {
+        return new GenericFastJsonRedisSerializer();
+    }
+
+
+    /**
+     * RedisTemplate配置
+     *
+     * @param factory
+     */
+    @Bean
+    public RedisTemplate<String, Object> redisTemplate(RedisConnectionFactory factory, RedisSerializer<String> redisKeySerializer, RedisSerializer<Object> redisValueSerializer) {
+        RedisTemplate<String, Object> redisTemplate = new RedisTemplate<>();
+        redisTemplate.setConnectionFactory(factory);
+
+        //设置Key的序列化采用StringRedisSerializer
+        redisTemplate.setKeySerializer(redisKeySerializer);
+        redisTemplate.setHashKeySerializer(redisKeySerializer);
+
+        //设置值的序列化
+        redisTemplate.setValueSerializer(redisValueSerializer);
+        redisTemplate.setHashValueSerializer(redisValueSerializer);
+
+        redisTemplate.afterPropertiesSet();
+
+        return redisTemplate;
+    }
+
+}
--- src/main/java/org/example/controller/distributelock/TestRedisBigKeyController.java
+++ src/main/java/org/example/controller/distributelock/TestRedisBigKeyController.java
@@ -1,14 +1,14 @@
 package org.example.controller.distributelock;
 
-import com.alibaba.fastjson.JSONObject;
+import com.alibaba.fastjson.JSON;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.example.entities.User;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-import redis.clients.jedis.Jedis;
 
 import java.util.List;
 
@@ -24,31 +24,36 @@ public class TestRedisBigKeyController {
     public static final Logger log = LogManager.getLogger(TestRedisBigKeyController.class);
 
     @Autowired
-    private Jedis jedis;
-
-    @Autowired
     private RedisTemplate redisTemplate;
 
     @RequestMapping("/setBigKey")
     public String setBigKey() {
         User user = User.builder().id(243).name("小萌").age(18).build();
         User ryan = User.builder().id(456).name("ryan").age(27).build();
-        //jedis.lpush("key", JSONObject.toJSONString(user), JSONObject.toJSONString(ryan));
-        redisTemplate.opsForList().leftPush("key", JSONObject.toJSONString(user));
+        redisTemplate.opsForList().leftPushAll("key1", user, ryan);
         return "redis 设置 bigkey list类型成功";
 
     }
 
-    @RequestMapping("/getBigKey")
-    public void getBigKey() {
-        List<String> list = jedis.lrange("key", 0, -1);
-        log.info("key 中值为：{}", list);
+    @RequestMapping("/getBigKey/{key}")
+    public void getBigKey(@PathVariable String key) {
+        List list = redisTemplate.opsForList().range(key, 0, -1);
+        log.info("list 的长度为：{}", list.size());
+        log.info("内容为：{}", list);
+        for (Object o : list) {
+            User user = JSON.parseObject(JSON.toJSONString(o), User.class);
+            System.out.println(user);
+        }
     }
 
     @RequestMapping("/removeOneElement")
     public void removeOneElement() {
-        List<String> key = jedis.lrange("key", 0, -1);
-        User ryan = User.builder().id(456).name("ryan").age(27).build();
-        jedis.lrem("key", 0, JSONObject.toJSONString(ryan));
+        String s1 = "123";
+        String s2 = "345";
+        String s3 = "123";
+        redisTemplate.opsForList().leftPushAll("key2",s1, s2, s3);
+
+        redisTemplate.opsForList().remove("key2", 1, "345");
+
     }
 }
