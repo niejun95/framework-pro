1b795bee9754eb9cefa45c239a3c3284db0b4538
niejun <1289680419@qq.com>
2022-08-31 23:42:18 +0800

    读取配置文件

---
+++ src/main/java/org/example/controller/TestReadPropertiesController.java
@@ -0,0 +1,37 @@
+package org.example.controller;
+
+import jdk.nashorn.internal.runtime.linker.LinkerCallSite;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * @ClassName: TestReadController
+ * @Author: ryan
+ * @CreateTime: 2022-08-31  22:27
+ * @Description: 读取 配置文件中 信息
+ * @Version: 1.0
+ */
+@RestController
+@PropertySource(value = "config.properties")
+public class TestReadPropertiesController {
+
+    @Value("${data.list}")
+    private String ref;
+
+    @RequestMapping("/read")
+    public void readProperties() {
+        System.out.println(ref);
+        List<String> list = new ArrayList<String>();
+        String[] arrs = ref.split(",");
+        for (String arr : arrs) {
+            list.add(arr);
+        }
+        System.out.println(list.size());
+
+    }
+}
