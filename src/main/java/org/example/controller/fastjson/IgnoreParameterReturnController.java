0a8f6d47d787df627a93fa8e37c983b3e9e51299
niejun95 <1289680419@qq.com>
2023-06-04 22:56:31 +0800

    fix: controller 忽略实体类中的某个属性不返回

--- src/main/java/org/example/controller/fastjson/IgnoreParameterReturnController.java
+++ src/main/java/org/example/controller/fastjson/IgnoreParameterReturnController.java
@@ -1,5 +1,7 @@
 package org.example.controller.fastjson;
 
+import com.alibaba.fastjson.JSON;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -12,15 +14,17 @@ import org.springframework.web.bind.annotation.RestController;
  */
 @RestController
 @RequestMapping("/ignore")
+@Slf4j
 public class IgnoreParameterReturnController {
 
-    @GetMapping
+    @GetMapping("/get")
     public IgnoreEntity get() {
         IgnoreEntity entity = new IgnoreEntity();
         entity.setId(1);
         entity.setName("ryan");
         entity.setGender("male");
         entity.setPhone("18156969092");
+        log.info("IgnoreEntity : {}", JSON.toJSONString(entity));
         return entity;
     }
 
