a3a981c63eec21b21be3ca8d2594d75e0e96f2cb
niejun <1289680419@qq.com>
2023-06-30 08:26:57 +0800

    fix: interceptor log request  info

--- src/main/java/org/example/interceptors/RequestBodyLogInterceptor.java
+++ src/main/java/org/example/interceptors/RequestBodyLogInterceptor.java
@@ -1,14 +1,17 @@
 package org.example.interceptors;
 
 import com.alibaba.fastjson.JSON;
+import com.alibaba.fastjson.serializer.SerializerFeature;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
+import org.aspectj.weaver.ast.Var;
 import org.jetbrains.annotations.NotNull;
 import org.springframework.web.servlet.HandlerInterceptor;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.util.Map;
+import java.util.stream.Collectors;
 
 /**
  * @author ryan
@@ -24,8 +27,8 @@ public class RequestBodyLogInterceptor implements HandlerInterceptor {
             throws Exception {
         String requestURI = request.getRequestURI();
         log.info("URI：{}", requestURI);
-        Map<String, String[]> map = request.getParameterMap();
-        log.info("Parameters：\n {}", JSON.toJSONString(map));
+        String body = request.getReader().lines().collect(Collectors.joining(System.lineSeparator()));
+        log.info("Body：\n {}", body);
         return true;
     }
 }
